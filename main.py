49388b6ad29c8dcdc45aef5fc0b5f268fb05ff30
JanvitaReddy11 <tamu2023@tamu.edu>
2024-06-18 22:08:22 -0500

    Added data split

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Winequality import logger
 from Winequality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Winequality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from Winequality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 #from Winequality.pipeline.stage_01_data_ingestion import *
 
 STAGE_NAME = "Data Ingestion Pipeline"
@@ -22,6 +23,20 @@ if __name__ == '__main__':
     try:
         logger.info(f"Stage {STAGE_NAME}  started")
         obj = DataValidationTrainingPipeline()
+        obj.main()
+
+        logger.info(f"Stage {STAGE_NAME} completed")
+    
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME = "Data Transformation Pipeline"
+if __name__ == '__main__':
+    try:
+        logger.info(f"Stage {STAGE_NAME}  started")
+        obj = DataTransformationTrainingPipeline()
         obj.main()
 
         logger.info(f"Stage {STAGE_NAME} completed")
---
+++ src/Winequality/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,30 @@
+from Winequality.config.configuration import ConfigurationManager
+from Winequality.components.data_transformation import DataTransformation
+from Winequality import logger
+from pathlib import Path
+
+STAGE_NAME = "Data Transformation stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            data_transformation_config = config.get_data_transformation_config()
+            data_transformation = DataTransformation(config=data_transformation_config)
+            data_transformation.train_test_spliting()
+        except Exception as e:
+            logger.error(f"Error in {STAGE_NAME}: {e}")
+            raise e
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(f"Exception occurred in {STAGE_NAME}")
+        raise e
