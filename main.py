653e51b621df908e19154a383d5d7cbc259a7c93
JanvitaReddy11 <tamu2023@tamu.edu>
2024-06-18 23:03:51 -0500

    Added model training process

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Winequality import logger
 from Winequality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Winequality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from Winequality.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from Winequality.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 #from Winequality.pipeline.stage_01_data_ingestion import *
 
 STAGE_NAME = "Data Ingestion Pipeline"
@@ -41,6 +42,18 @@ if __name__ == '__main__':
 
         logger.info(f"Stage {STAGE_NAME} completed")
     
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME = "Model Training Pipeline"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
